3a483b923142c4dd06320d69f5bbcf69e2a59b54
DhruvKaushal <dhruvkaushal1997@gmail.com>
2020-09-18 14:53:14 +0530

    task 1

--- Day 4/customerAccount.java
+++ Day 4/customerAccount.java
@@ -11,11 +11,12 @@ public class customerAccount extends person {
     }
     @Override
     public void displayInfo(){
-        System.out.println("-------------Student data-------------");
+        System.out.println("-------------Customer data-------------");
         System.out.println("Name: "+ getName());
         System.out.println("Gender: "+ getGender());
         System.out.println("Phone :" + getPhone());
-        //System.out.println("City: "+ this.account.balance + " State: "+this.account.state);
+        System.out.println("ID :" + custNo);
+        System.out.println("Balance: "+ account.balance + " Account No: "+account.accountNo);
         System.out.println("DOB: "+ getDOB());
         addDetails();
     }
